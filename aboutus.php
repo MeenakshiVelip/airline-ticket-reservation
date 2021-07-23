0442e501f3caa31b0c703051e3013919011d22f5
MeenakshiVelip <meenakshivelip@klsvdit.edu.in>
2021-07-23 22:33:04 +0530

    initial commit

---
+++ aboutus.php
@@ -0,0 +1,57 @@
+<?php
+	session_start();
+?>
+<html>
+	<head>
+		<title>
+			Account Login
+		</title>
+		<style>
+			input {
+    			border: 1.5px solid #030337;
+    			border-radius: 4px;
+    			padding: 7px 30px;
+			}
+			input[type=submit] {
+				background-color:#030337 ;
+				color:white;
+    			border-radius: 4px;
+    			padding: 7px 45px;
+    			margin: 0px 60px
+			}
+			h4{text-align:center;}
+		</style>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<link rel="stylesheet" href="font-awesome-4.7.0\css\font-awesome.min.css">
+	</head>
+	<body>
+		<img class="logo" src="images/air.jpg"/> 
+		<h1 id="title" style="color:blue;">
+			INDIAN AIRLINES
+			
+		</h1>
+		<div>
+			<ul>
+				<li><a href="home_page.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+				<li><a href="contactus.php"><i class="fa fa-phone" aria-hidden="true"></i> Contact Us</a></li>
+			</ul>
+		</div>
+		</div>
+		<div class="container">
+			<div class="welcome_text" style="color:black;"><h4 style="color:black;">Welcome to INDIAN Airlines!</h4><p></p>
+			<h6 style="color:black;">Indian Airlines, later Indian, was a major Indian airline based in Delhi and focused primarily on domestic routes, along with several international services to neighbouring countries in Asia. 
+			It was state-owned after merger of eight pre-Independence domestic airlines and was administered by the Ministry of Civil Aviation. 
+			Indian was one of the two flag carriers of India, the other being Air India.<p></p>
+            On 7 December 2005, the airline was rebranded as Indian for advertising purposes as a part of a program to revamp its image in preparation for an initial public offering (IPO).
+			The airline operated closely with Air India, India's national overseas carrier.
+			Alliance Air was a fully owned subsidiary of Indian.<p></p>
+			In 2007, the Government of India announced that Indian would be merged into Air India. 
+			As part of the merger process, a new company called the National Aviation Company of India Limited (now called Air India Limited) was established, into which both Air India (along with Air India Express) and Indian (along with Alliance Air) would be merged. 
+			Once the merger was completed, the airline – called Air India – would continue to be headquartered in Mumbai and would have a fleet of over 130 aircraft.</h6><p></p>
+			<p></p>
+			</div>
+			<img src="images/air.jpg" width=100%>
+		</div>
+</form>
+	</body>
+</html>
---
+++ contactus.php
@@ -0,0 +1,60 @@
+<?php
+	session_start();
+?>
+<html>
+	<head>
+		<title>
+			Account Login
+		</title>
+		<style>
+			input {
+    			border: 1.5px solid #030337;
+    			border-radius: 4px;
+    			padding: 7px 30px;
+			}
+			input[type=submit] {
+				background-color: #030337;
+				color: white;
+    			border-radius: 4px;
+    			padding: 7px 45px;
+    			margin: 0px 60px
+			}
+		</style>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<link rel="stylesheet" href="font-awesome-4.7.0\css\font-awesome.min.css">
+	</head>
+	<body>
+		<img class="logo" src="images/air.jpg"/> 
+		<h1 id="title" style="color:blue;">
+			INDIAN AIRLINES
+			
+		</h1>
+		<div>
+			<ul>
+				<li><a href="home_page.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+			
+			</ul>
+		</div>
+		</div>
+		<div class="container">
+			<div class="welcome_text" style="color:black;">Welcome to INDIAN Airlines!<p></p>
+	Call 24x7 Customer Care numbers 0124-2641407 / 020-26231407 / 1860 233 1407 from MTNL / BSNL lines and Mobile / landlines of most private telecom operators in India only.<p></p>
+    Air India contact numbers for callers outside India<p></p>
+    USA & Canada 1888 634 1407 (Toll Free)<p></p>
+    UK           8000 489 254 (Toll Free)<p></p>
+    Australia    613 701 98270 (Toll Number) #<p></p>
+    France       331 874 06644 (Toll Number) #<p></p>
+    Germany      496 9255 11337 (Toll Number) #<p></p>
+    Singapore    800 101 4016 (Toll Free)<p></p>
+    # Calls charged to callers as per applicable network call rates.<p></p>
+    Callers from all foreign locations can also call on<p></p>
+       + 91 124 2641407 (International call rates applicable)<p></p>
+       + 91 20 2623 1407 (International call rates applicable)<p></p>
+    Call Centre Email IDs<p></p>
+    E-mail ID :contactus@airindia.in<p></p>
+	</div>
+	<img src="images/air.jpg" width=120%>
+		</div>
+</form>
+	</body>
+</html>
